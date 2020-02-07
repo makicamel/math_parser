dd69449f7c28101e19eb45c88d8a1098f23dcc48
makicamel <unright@gmail.com>
2020-02-08 01:52:06 +0900

    Change tree from func_call to multiple when method_missing

--- main.rb
+++ main.rb
@@ -45,14 +45,20 @@ module MyCalcurator
       genv[tree[1]] = ["user_defined", tree[2], tree[3]]
     when "func_call"
       args = tree[2..].map { |t| evaluate(t, genv, lenv) }
+      # 組み込み関数
       if respond_to?(tree[1]) || Kernel.respond_to?(tree[1])
         send(tree[1], *args)
-      else
+      # ユーザ定義関数
+      elsif genv.has_key? tree[1]
         func_lenv = {}
         mhd = genv[tree[1]]
         params = mhd[1]
         params.each_with_index { |param, j| func_lenv[param] = args[j] }
         evaluate(mhd[2], genv, func_lenv)
+      # 未定義の場合は tree を変更して再度 evaluate する
+      else
+        new_tree = ["*", ["var_ref", tree[1]], tree[2]]
+        evaluate(new_tree, genv, lenv)
       end
     when "if"
       if evaluate(tree[1], genv, lenv)
